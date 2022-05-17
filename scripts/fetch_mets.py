8f2d13d45e6112c5e267957879a4ae07a5aad459
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-17 13:35:28 +0200

    first try for #1 (breaks dockerized wf)

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -5,4 +5,4 @@ user = os.environ.get('TR_USER')
 pw = os.environ.get('TR_PW')
 col_id = os.environ.get('COL_ID')
 
-mpr_docs = transkribus_utils.trp_collection_to_mets(col_id, user, pw, file_path='/data/mets')
+mpr_docs = transkribus_utils.trp_collection_to_mets(col_id, user, pw, file_path='./mets')
