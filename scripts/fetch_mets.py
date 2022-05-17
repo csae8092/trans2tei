242decef5eab929e61a777c763986bd24c892cce
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-17 14:23:21 +0200

    its a class now

--- scripts/fetch_mets.py
+++ scripts/fetch_mets.py
@@ -1,8 +1,14 @@
 import os
-from transkribus_utils import transkribus_utils
+from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
 user = os.environ.get('TR_USER')
 pw = os.environ.get('TR_PW')
 col_id = os.environ.get('COL_ID')
 
-mpr_docs = transkribus_utils.trp_collection_to_mets(col_id, user, pw, file_path='./mets')
+transkribus_client = ACDHTranskribusUtils(
+    user=user,
+    password=pw,
+    transkribus_base_url="https://transkribus.eu/TrpServer/rest"
+)
+
+mpr_docs = transkribus_client.collection_to_mets(col_id, file_path='./mets')
